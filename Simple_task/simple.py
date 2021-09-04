bfbc85da3a0d0138e9d488d8a19b410aeb878296
Sammels Favillion <sasagete@gmail.com>
2021-09-04 20:19:31 +0300

    Make branch for simple tasks

---
+++ Simple_task/simple.py
@@ -0,0 +1,29 @@
+# Практика числа
+a = 2
+b = 2.5 - a
+print(a + b)
+
+
+# Практика строки
+name = "Sammels".upper()
+print (f'Привет, {name}')
+
+
+# Практика числа ч.2
+v = input('Введите число от 1 до 10: ')
+add_ten = 10 + int(v)
+print(add_ten)
+
+
+# Практика строки ч.2
+input_name = input('Введите ваше имя: ')
+print('Привет ', input_name.upper(),'! ', 'Как дела?')
+
+
+# Практика приведение типов
+print(float('1'))
+print(int(abs(float('2.5'))))
+print(bool(1))
+print(bool(''))
+print(bool(0))
+
