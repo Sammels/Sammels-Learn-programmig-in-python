cb85485a677c49c3103abae11496972c2b0b78b2
Sammels <Sasagete@gmail.com>
2021-09-05 16:38:22 +0300

    Update function.py
    Ели будут цифры, то мы их приведем к строковому типу, соединим строки.

--- Simple_task/function.py
+++ Simple_task/function.py
@@ -2,7 +2,7 @@
 
 # Задача 1
 def get_summ(one,two,delimeter='&'):
-	one_plus_two = one + delimeter + two
+	one_plus_two = str(one) + delimeter + str(two)
 	print(one_plus_two.upper())
 
 get_summ("Learn", "python")
